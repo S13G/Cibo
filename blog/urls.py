b1dfd9bad9fecc6338f4708976c730def118de81
s13g <ayflix0@gmail.com>
2022-10-16 12:24:32 +0100

    added rss to the blog website

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ import site
 from django.urls import path
 
 from . import views
+from blog.feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -12,4 +13,5 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
